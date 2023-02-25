a6a34cee82bef14fd456e6f24f4855ef7dae7f7a
RicardoSampaio12121 <ricardo_cs@outlook.pt>
2023-02-25 20:38:52 +0000

    Basic solution structure

---
+++ Frontend/app/src/main/java/com/RicardoSampaio/shoppingcart/GenericPage/GenericPageActivity.kt
@@ -0,0 +1,11 @@
+package com.RicardoSampaio.shoppingcart.GenericPage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GenericPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generic_page)
+    }
+}
